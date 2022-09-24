6050c9bf2d4eebf27c72643c154b618e3887a590
anibal <anibalgarcia4@gmail.com>
2022-09-24 11:32:07 -0300

    arreglando categorias

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
 
             <Route path='/' element={<ItemListContainer greeting='CATALOGO DE PRODUCTOS'/>} />
-            <Route path='/category/categoryid' element={ <ItemListContainer />} />
+            <Route path='/category/:categoryid' element={ <ItemListContainer />} />
             <Route path='/item/:id' element={ <ItemDetailContainer/> } />
             <Route path='/cart' element={ <Cart/> } />
            
